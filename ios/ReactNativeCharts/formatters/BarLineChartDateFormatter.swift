b983a3c99fbbc1a800bf0b68b3350cbc17ef9809
lewie9021 <lb.barnes@live.co.uk>
2019-01-20 08:41:55 +0000

    Refactoring.

--- ios/ReactNativeCharts/formatters/BarLineChartDateFormatter.swift
+++ ios/ReactNativeCharts/formatters/BarLineChartDateFormatter.swift
@@ -1,8 +1,8 @@
 //
-//  ChartDateFormatter.swift
+//  BarLineChartDateFormatter.swift
 //  Charts
 //
-//  Created by lewie9021 on 2019/01/19.
+//  Created by Lewis Barnes (lewie9021) on 2019/01/19.
 //
 
 import Foundation
@@ -27,18 +27,18 @@ open class BarLineChartDateFormatter: NSObject, IValueFormatter, IAxisValueForma
     
     fileprivate func convertValueToMilliseconds(_ value: Double, _ timeUnit: String) -> Double {
         switch timeUnit {
-        case "MILLISECONDS":
-            return value / 1000.0
-        case "SECONDS":
-            return value;
-        case "MINUTES":
-            return value * 60
-        case "HOURS":
-            return value * 60 * 60
-        case "DAYS":
-            return value * 60 * 60 * 24
-        default:
-            return value / 1000.0
+            case "MILLISECONDS":
+                return value / 1000.0
+            case "SECONDS":
+                return value;
+            case "MINUTES":
+                return value * 60
+            case "HOURS":
+                return value * 60 * 60
+            case "DAYS":
+                return value * 60 * 60 * 24
+            default:
+                return value / 1000.0
         }
     }
     
@@ -50,7 +50,7 @@ open class BarLineChartDateFormatter: NSObject, IValueFormatter, IAxisValueForma
     }
     
     fileprivate func format(_ value: Double) -> String {
-        let valueInMilliseconds = convertValueToMilliseconds(value, self.timeUnit);
+        let valueInMilliseconds = convertValueToMilliseconds(value, timeUnit);
         let date = Date(timeIntervalSince1970: valueInMilliseconds);
         let second = 1000.0;
         let minute = second * 60.0;
@@ -58,23 +58,22 @@ open class BarLineChartDateFormatter: NSObject, IValueFormatter, IAxisValueForma
         let day = hour * 24.0;
         let month = day * 30.0;
         
-        let chartXAxis = self.chart.xAxis;
+        let xAxis = chart.xAxis;
         let yearText = String(getFormattedDate(date, "yyyy").suffix(2));
         let monthText = getFormattedDate(date, "MMM");
         
         if (chart.visibleXRange > month * 6.0) {
-            chartXAxis.labelCount = 5;
-            chartXAxis.axisMaxLabels = 5;
+            xAxis.labelCount = 5;
+            xAxis.axisMaxLabels = 5;
             
             return monthText + " '" + yearText;
         } else {
-            chartXAxis.labelCount = 3;
-            chartXAxis.axisMaxLabels = 3;
+            xAxis.labelCount = 3;
+            xAxis.axisMaxLabels = 3;
             
             let dayText = getFormattedDate(date, "d");
             
             return dayText + " " + monthText + " '" + yearText;
         }
     }
-    
 }
